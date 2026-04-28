50cac4aac8e54b0b4f23b488cdef831165a3e0db
Toni-696 <jarivilla@hotmail.com>
2026-04-28 20:34:22 +0200

    implementacion CORS para que conecte con React

--- src/main/java/com/toni/safememories/config/SecurityConfig.java
+++ src/main/java/com/toni/safememories/config/SecurityConfig.java
@@ -14,6 +14,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 
 //ESTA CLASE ES EL PORTERO DE LA APP, decide quien entra sin problema, quien se tiene que identificar y
@@ -46,6 +51,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())//desactiva una seguridad de Spring que se suele usar en formularios
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(jwtAuthenticationEntryPoint))
@@ -54,10 +60,25 @@ public class SecurityConfig {
                         .requestMatchers("/usuarios/registro", "/usuarios/login").permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults())//no se va a usar, es el
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);//antes del filtro normal de usuario/contraseña,
-                // pasa por mi filtro JWT
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        //antes del filtro normal de usuario/contraseña,
+        // pasa por mi filtro JWT
 
         return http.build();//termina la configuración y construye la seguridad final
+    }
+
+    @Bean //permite que se conecten diferentes origenes (8080 backend con 5173 front)
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
